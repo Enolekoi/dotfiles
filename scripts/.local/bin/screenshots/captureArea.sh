0ddabdc9329bc0c201682dc94133506eb9948d21
Enolekoi <fc.severin@protonmail.com>
2024-02-15 22:12:52 +0100

    added screenshot functionality

--- scripts/.local/bin/screenshots/captureArea.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-grim -g "$(slurp)" $HOME/Bilder/Screenshots/$(date +'%s_screenshot.png')
-
-play $HOME/.local/share/hypr/sounds/camera-shutter.ogg
--- scripts/.local/bin/screenshots/captureScreen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#!/bin/bash
-
-grim -g "$(slurp -o)" $HOME/Bilder/Screenshots/$(date +'%s_screenshot.png')
-
-play $HOME/.config/hypr/assets/sounds/camera-shutter.ogg
